0c016df538db2f467ccc797e4c1a2e5b46fe4cee
Gabriel Favero <gabriel290974@gmail.com>
2025-11-11 23:23:19 -0300

    Adicionando comando de informações do bot

--- cogs/geral.py
+++ cogs/geral.py
@@ -1,4 +1,6 @@
 import discord
+from embed import error, success, default
+from datetime import date
 from discord.ext import commands
 from discord import app_commands
 
@@ -21,6 +23,24 @@ class Geral(commands.Cog, name="Geral"):
         import random
         num = random.randint(1, 100)
         await interaction.response.send_message(f"Seu número aleatório é: {num}")
-
+    
+    @commands.command(help="Informações sobre o bot", aliases=["info", "about"])
+    async def sobre(self, ctx):
+        app_info = await ctx.bot.application_info()
+        data_criacao = ctx.bot.user.created_at.strftime("%d/%m/%Y %H:%M:%S")
+        owner = app_info.owner
+        embed = default.DefaultEmbed.create(
+            title="🤖 Sobre o Bot",
+            description="Olá! Eu sou o **Rodolfo**, um bot do Discord criado para ajudar você com várias tarefas e comandos divertidos!\n\n"
+            "Aqui estão algumas informações sobre mim:",
+        )
+        embed.set_thumbnail(url=app_info.icon.url if app_info.icon else ctx.bot.user.display_avatar.url)
+        
+        embed.add_field(name="Data de Criação", value=f"{data_criacao}", inline=False)
+        embed.add_field(name="👑 Desenvolvedor", value=f"{owner.name}", inline=True)
+        embed.add_field(name="🌐 Público?", value="✅ Sim" if app_info.bot_public else "🔒 Não", inline=True)
+        
+        embed.set_footer(text=f"{date.today().year} Rodolfo Bot ©")
+        await ctx.send(embed=embed)
 async def setup(bot):
     await bot.add_cog(Geral(bot))
