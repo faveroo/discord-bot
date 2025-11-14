f0188667959a96e23e89cf4d6435e03bba8bf4cb
Gabriel Favero <gabriel290974@gmail.com>
2025-11-14 01:21:06 -0300

    implementando tratamento do erro MissingPermissions e adicionando embed no CommandNotFound

--- handlers/global_errors.py
+++ handlers/global_errors.py
@@ -5,18 +5,34 @@ import discord
 async def setup_global_error_handler(bot):
     @bot.event
     async def on_command_error(ctx, error):
+
+        traducao = {
+            "Manage Messages": "Gerenciar mensagens",
+            "Ban Members": "Banir membros",
+            "Kick Members": "Expulsar membros",
+            "Moderate Members": "Moderar membros",
+            "Manage Guild": "Gerenciar Guilda/Servidor",
+        }
+
         if hasattr(ctx.command, 'on_error'):
             return
         
         if ctx.cog:
             if ctx.cog._get_overridden_method(ctx.cog.cog_command_error) is not None:
                 return
-        
-        if isinstance(error, commands.MissingRequiredArgument):
+        if isinstance(error, commands.MissingPermissions):
+            permissoes = [perm.replace('_', ' ').title() for perm in error.missing_permissions]
+            lista = ', '.join(traducao.get(p, p) for p in permissoes)
+            await ctx.send(f"🚫 Você precisa das permissões: **{lista}** para usar este comando.")
+        elif isinstance(error, commands.MissingRequiredArgument):
             embed = ErrorEmbed.create(
                 title="❌ Argumento faltando",
                 description=f"Verifique os argumentos com `{ctx.prefix}help {ctx.command}`.",
             )
             await ctx.send(embed=embed)
         elif isinstance(error, commands.CommandNotFound):
-            return
+            embed = ErrorEmbed.create(
+                title="Comando não encontrado",
+                description=f"Verifique os comandos com `{ctx.prefix}help`.",
+            )
+            await ctx.send(embed=embed) 
