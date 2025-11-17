63fe6eb05309feeec4975f1f6fafb412f69cf627
Gabriel Favero Hoffmann <gabriel290974@gmail.com>
2025-11-17 09:48:56 -0300

    colocando carinha triste

--- embed/error.py
+++ embed/error.py
@@ -5,5 +5,5 @@ class ErrorEmbed:
     @staticmethod
     def create(title: str, description: str = "", color: discord.Color = discord.Color.red()) -> discord.Embed:
         embed = discord.Embed(title=title, description=description, color=color)
-        embed.set_footer(text=f"{date.today().year}")
+        embed.set_footer(text=f"🙁 -{date.today().year}")
         return embed
