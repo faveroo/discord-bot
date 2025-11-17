ef156cbb4d16b4a6d2e0f8b669dd2d1811e956cb
Gabriel Favero Hoffmann <gabriel290974@gmail.com>
2025-11-17 09:31:12 -0300

    Correção do footer

--- embed/default.py
+++ embed/default.py
@@ -5,5 +5,5 @@ class DefaultEmbed:
     @staticmethod
     def create(title: str, description: str = "", color: discord.Color = discord.Color.dark_purple()) -> discord.Embed:
         embed = discord.Embed(title=title, description=description, color=color)
-        embed.set_footer(date.today().year)
+        embed.set_footer(text=f"{date.today().year}")
         return embed
--- embed/error.py
+++ embed/error.py
@@ -5,5 +5,5 @@ class ErrorEmbed:
     @staticmethod
     def create(title: str, description: str = "", color: discord.Color = discord.Color.red()) -> discord.Embed:
         embed = discord.Embed(title=title, description=description, color=color)
-        embed.set_footer(date.today().year)
+        embed.set_footer(text=f"{date.today().year}")
         return embed
