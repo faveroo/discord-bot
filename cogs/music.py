f5cff4c79fd949382674eacd770325b7356482f6
Gabriel Favero <gabriel290974@gmail.com>
2025-11-20 18:10:58 -0300

    Creating commands to resume, pause, and control the volume, as well as implementing a queue for the songs.

--- cogs/music.py
+++ cogs/music.py
@@ -1,12 +1,16 @@
 import discord
 import yt_dlp
 import asyncio
+from embed import error, default, success, info
+from discord import PCMVolumeTransformer
 from discord.ext import commands
 
 
 class Music(commands.Cog, name="Músicas"):
     def __init__(self, bot:commands.Bot):
         self.bot = bot
+        self.queues = {}
+        self.volume = {}
 
     async def search_ytdlp_async(self, query, ydl_options):
         loop = asyncio.get_running_loop()
@@ -15,6 +19,38 @@ class Music(commands.Cog, name="Músicas"):
     def _extract(self, query, ydl_options):
         with yt_dlp.YoutubeDL(ydl_options) as ydl:
             return ydl.extract_info(query, download=False)
+
+    def play_next(self, ctx, vc):
+        guild_id = ctx.guild.id
+
+        if guild_id not in self.queues or len(self.queues[guild_id]) == 0:
+            return
+
+        track = self.queues[guild_id].pop(0)
+
+        raw = discord.FFmpegPCMAudio(
+            track["url"],
+            before_options="-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
+            options='-vn -filter:a "volume=0.2"',
+            executable="bin\\ffmpeg\\ffmpeg.exe"
+        )
+
+        source = PCMVolumeTransformer(raw, volume=self.volume.get(guild_id, 0.5))
+        vc.play(source, after=lambda e: self.play_next(ctx, vc))
+
+        asyncio.run_coroutine_threadsafe(
+            ctx.send(f"▶️ Tocando: **{track['title']}**"),
+            self.bot.loop
+        )
+
+    @commands.command(name="leave", help="Retira o bot da call", aliases=["sair"])
+    async def sair(self, ctx):
+        vc = ctx.voice_client
+        if not vc or not vc.is_connected():
+            return await ctx.send("Não estou em nenhuma call aqui.")
+
+        await vc.disconnect()
+        await ctx.send("Saí da call.")
 
     @commands.command(name="play", aliases=["tocar"], usage="<musica>")
     async def play(self, ctx:commands.Context, *, song_query: str):
@@ -49,16 +85,20 @@ class Music(commands.Cog, name="Músicas"):
         audio_url = first_track["url"]
         title = first_track.get("title", "Untitled")
 
-        ffmpeg_options = {
-            "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
-            "options": "-vn"
-        }
-        
-        source = discord.FFmpegOpusAudio(audio_url, **ffmpeg_options, executable="bin\\ffmpeg\\ffmpeg.exe")
+        guild_id = ctx.guild.id
+        if guild_id not in self.queues:
+            self.queues[guild_id] = []
 
-        vc_client.play(source)
-        await ctx.send(f"▶️ Tocando: **{title}**")
+        # adiciona na fila
+        self.queues[guild_id].append({
+            "url": audio_url,
+            "title": title
+        })
 
+        if not vc_client.is_playing():
+            self.play_next(ctx, vc_client)
+        else:
+            await ctx.send(f"➕ Adicionado à fila: **{title}**")
 
     @commands.command(name="pause", help="Pausa a música", aliases=["pausar", "p"])
     async def pausar(self, ctx:commands.Context):
@@ -83,5 +123,73 @@ class Music(commands.Cog, name="Músicas"):
         voice_client.pause()
         return await ctx.send("⏸️ Pausado")
 
+    @commands.command(name="resume", help="Despausa uma música", aliases=["despausar"])
+    async def resume(self, ctx):
+        if ctx.author.voice is None:
+            return await ctx.send("❌ Você precisa estar conectado em um canal de voz")
+        
+        vc = ctx.author.voice.channel
+        
+        voice_client = ctx.guild.voice_client
+
+        if not voice_client:
+            return await ctx.send("❌ Não estou conectado a um canal de voz")
+        
+        if voice_client.is_playing():
+            return await ctx.send("❌ A música não está pausada.")
+
+        voice_client.resume()
+        return await ctx.send("▶️ Voltando a reproduzir...")
+
+    @commands.command(name="skip", aliases=["next", "pular"])
+    async def skip(self, ctx):
+        vc = ctx.guild.voice_client
+
+        if not vc or not vc.is_connected():
+            return await ctx.send("❌ Não estou conectado a um canal de voz.")
+
+        if not vc.is_playing():
+            return await ctx.send("⚠ Nada está tocando.")
+
+        vc.stop()
+        await ctx.send("⏭ Pulando...")
+
+    @commands.command(name="stop", aliases=["parar", "end"])
+    async def stop(self, ctx):
+        vc = ctx.guild.voice_client
+
+        if not vc or not vc.is_connected():
+            return await ctx.send("❌ Não estou conectado a um canal de voz.")
+
+        guild_id = ctx.guild.id
+
+        self.queues[guild_id] = []
+
+        vc.stop()
+
+        await ctx.send("🛑 Música parada e fila limpa.")
+
+    @commands.command(name="volume", aliases=["vol"])
+    async def volume(self, ctx, *, vol: int = None):
+        if vol is None:
+            guild_volume = self.volume.get(ctx.guild.id, 0.5)
+            guild_volume *= 100
+
+            embed = default.DefaultEmbed.create(
+                title=f"O volume atual é: {int(guild_volume)}"
+            )
+            return await ctx.send(embed=embed)
+
+        if not 0 <= vol <= 200:
+            return await ctx.send("O volume deve ser entre 0 e 200.")
+        
+        guild_id = ctx.guild.id
+        self.volume[guild_id] = vol / 100
+
+        vc_client = ctx.voice_client
+        if vc_client and vc_client.source:
+            vc_client.source.volume = self.volume[guild_id]
+        
+        await ctx.send(f"🔊 Volume definido para **{vol}%**")
 async def setup(bot:commands.Bot):
     await bot.add_cog(Music(bot))
