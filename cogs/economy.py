bc32d772af1ccfba36e5af250584934b7e8d2340
Gabriel Favero Hoffmann <gabriel290974@gmail.com>
2025-11-21 11:20:39 -0300

    Adicionado comando de aposta

--- cogs/economy.py
+++ cogs/economy.py
@@ -1,6 +1,8 @@
 import discord
 import random
+import asyncio
 from discord.ext import commands
+from helpers import near
 from embed import error, success, default
 from datetime import datetime, timedelta, timezone
 from database import get_currency, update_currency, get_last_daily, set_last_daily
@@ -17,7 +19,6 @@ class Economy(commands.Cog, name="Economia"):
     
     @commands.command(name="balance", help="Ver seu saldo atual 💰", aliases=["saldo", "money"])
     async def balance(self, ctx, member: discord.Member = None):
-        from database import get_currency
         target = member or ctx.author
         saldo = await get_currency(target)
 
@@ -128,6 +129,98 @@ class Economy(commands.Cog, name="Economia"):
             description=f"Você recebeu {rewards_amount} moedas como recompensa diária."
         )
         await ctx.send(embed=embed)
-        
+     
+    @commands.command(name="bet", help="Aposta com um usuário", aliases=["apostar"])
+    async def bet(self, ctx, member: discord.Member = None, value: int = None):
+            if member == ctx.author:
+                return await ctx.send(embed=error.ErrorEmbed.create(
+                    title="❌ Erro",
+                    description="Você não pode transferir moedas para si mesmo."
+                ))
+                
+            
+            if member is None:
+                return await ctx.send(embed=error.ErrorEmbed.create(
+                    title="❌ Erro",
+                    description="Usuário não encontrado."
+                ))
+            
+            if value is None or value <= 0:
+                return await ctx.send(embed=error.ErrorEmbed.create(
+                    title="❌ Valor inválido",
+                    description="Use: !bet @usuario 100"
+                ))
+                
+            user_currency = get_currency(ctx.author)
+            enemy_currency = get_currency(member)
+            
+            if user_currency < value:
+                return await ctx.send(embed=error.ErrorEmbed.create(
+                    title="❌ Erro",
+                    description="Você não possui moedas o suficiente"
+                ))
+                     
+            if enemy_currency < value:
+                return await ctx.send(embed=error.ErrorEmbed.create(
+                    title="❌ Erro",
+                    description="O outro usuário não possui moedas o suficiente"
+                ))
+            
+            await ctx.send(f"{ctx.author.mention} e {member.mention}, escolham um número entre 1 e 100!")
+            
+            async def wait(user):
+                def check(msg):
+                    return msg.author == user and msg.channel == ctx.channel
+                try:
+                    msg = await self.bot.wait_for("message", check=check, timeout=30)
+                    await ctx.send(f"{user} escolheu {msg.content}")
+                    return user, msg.content
+                except asyncio.TimeoutError:
+                    return user, None
+                
+            await ctx.send(f"{ctx.author.mention}, envie sua escolha em até **30 segundos**!")
+
+            resposta1 = await wait(ctx.author)
+
+            if resposta1[1] is None:
+                return await ctx.send(f"❌ {ctx.author.display_name} não respondeu. A aposta foi cancelada.")
+
+            await ctx.send(f"✔ {ctx.author.display_name} respondeu!\n"
+                        f"{member.mention}, agora é sua vez!")
+
+            resposta2 = await wait(member)
+
+            if resposta2[1] is None:
+                return await ctx.send(f"❌ {member.display_name} não respondeu. A aposta foi cancelada.")
+            
+            author, escolha1 = resposta1
+            alvo, escolha2 = resposta2
+            
+            try:
+                escolha1 = int(escolha1)
+                escolha2 = int(escolha2)
+            except ValueError:
+                return await ctx.send("❌ Ambos devem digitar apenas números!")
+            
+            if not (1 <= escolha1 <= 100 and 1 <= escolha2 <= 100):
+                return await ctx.send("❌ Os números devem estar entre 1 e 100!")
+            
+            if escolha1 == escolha2:
+                return await ctx.send("❌ Ambos escolheram o mesmo número! Escolham números diferentes.")
+
+            players = {
+                escolha1: author,
+                escolha2: alvo
+            }
+            
+            rdn = random.randint(1, 100)
+            winner, loser = near(escolha1, escolha2, rdn)
+            winner_user = players[winner]
+            loser_user = players[loser]
+            
+            await ctx.send(f"🎯 Número sorteado: **{rdn}**\n🏆 O vencedor é: **{winner_user.mention}**!")
+            await update_currency(loser_user, -value)
+            await update_currency(winner_user, value)
+            
 async def setup(bot):
     await bot.add_cog(Economy(bot))
