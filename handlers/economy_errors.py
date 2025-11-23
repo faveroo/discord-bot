ddb51f06bebedd1e76ee3ab3420e7ecf61692eea
Gabriel Favero <gabriel290974@gmail.com>
2025-11-23 03:35:29 -0300

    Added errors to economy_errors.py

--- handlers/economy_errors.py
+++ handlers/economy_errors.py
@@ -10,3 +10,38 @@ class EconomyError:
                 title="❌ Erro",
                 description="Usuário não encontrado."
             ))
+    
+    @staticmethod
+    async def InvalidAmount(ctx):
+        return await ctx.send(embed=ErrorEmbed.create(
+            title="❌ Erro",
+            description="A quantia não pode ser negativa."
+        ))
+    
+    @staticmethod
+    async def NotEnoughMoney(ctx):
+        return await ctx.send(embed=ErrorEmbed.create(
+            title="❌ Erro",
+            description="Você não tem moedas o suficiente."
+        ))
+    
+    @staticmethod
+    async def NotEnoughMoneyEnemy(ctx):
+        return await ctx.send(embed=ErrorEmbed.create(
+            title="❌ Erro",
+            description="O outro usuário não tem moedas o suficiente."
+        ))
+    
+    @staticmethod
+    async def InvalidNumber(ctx, min: int, max: int):
+        return await ctx.send(embed=ErrorEmbed.create(
+            title="❌ Erro",
+            description=f"O número deve estar entre {min} e {max}."
+        ))
+    
+    @staticmethod
+    async def InvalidBetType(ctx):
+        return await ctx.send(embed=ErrorEmbed.create(
+            title="❌ Erro",
+            description="Tipo de aposta inválido. Use: red, black, 1-12, 13-24, 25-36, 1-18, 19-36, ou números 0-36."
+        ))
