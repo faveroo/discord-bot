89b9dfa3ebe7d89982299915c54c329dd16fb3aa
Gabriel Favero <gabriel290974@gmail.com>
2025-11-23 00:32:49 -0300

    Alterando cor do embed de infos

--- embed/info.py
+++ embed/info.py
@@ -3,6 +3,6 @@ from datetime import date
 
 class InfoEmbed:
     @staticmethod
-    def create(title: str, description: str = "", color: discord.Color = discord.Color.blue()) -> discord.Embed:
+    def create(title: str, description: str = "", color: discord.Color = discord.Color.gold()) -> discord.Embed:
         embed = discord.Embed(title=title, description=description, color=color)
         return embed
