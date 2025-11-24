a2299a64688a4a7aee563eb3456d1d58b8732e9f
Gabriel Favero <gabriel290974@gmail.com>
2025-11-24 07:27:46 -0300

    arquivo para carregar bandeiras

---
+++ helpers/flags.py
@@ -0,0 +1,9 @@
+FLAGS = {
+    "brasil": "https://flagcdn.com/w320/br.png",
+    "portugal": "https://flagcdn.com/w320/pt.png",
+    "argentina": "https://flagcdn.com/w320/ar.png",
+    "japao": "https://flagcdn.com/w320/jp.png",
+    "alemanha": "https://flagcdn.com/w320/de.png",
+    "franca": "https://flagcdn.com/w320/fr.png",
+    "espanha": "https://flagcdn.com/w320/es.png",
+}
