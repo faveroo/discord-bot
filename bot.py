e64afabcaaf077a0117eecebad098db47dae7be2
Gabriel Favero <gabriel290974@gmail.com>
2025-11-23 21:06:42 -0300

    Adicionado intents de presença para comando de userinfo

--- bot.py
+++ bot.py
@@ -33,6 +33,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.guilds = True
 intents.members = True
+intents.presences = True
 intents.reactions = True  
 intents.messages = True
 intents.bans = True
